d5dbe9d3d68ca29defd55a71d4cccdfaacfae6d1
TheNew000 <mahkatorocks@gmail.com>
2017-02-02 17:44:11 -0500

    added class functions

--- src/index.js
+++ src/index.js
@@ -1,9 +1,39 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
+import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+const API_KEY = 'AIzaSyDU6EmqpUmvTiwjIZDIayOrd0Kev8Y2WxM';
 
-// Create a new component which produces HTML
-const App = () => {
-    return <div>Hi!</div>;
+// Create a new functional component which produces HTML:
+// const App = () => {
+//     return (
+//         <div>
+//             <SearchBar />
+//         </div>
+//     );
+// };
+
+class App extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = { videos: [] };
+
+        YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+            this.setState({ videos });
+            // this.setState({ videos: videos });
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <SearchBar />
+                <VideoList videos={this.state.videos} />
+            </div>
+        );
+    }
 }
 
 // Take this component and put it into the DOM
